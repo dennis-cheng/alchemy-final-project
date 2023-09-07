33f72bbcd7a43e813a63884e44ff63c95867e0d3
Dennis Cheng <dennischeng@hotmail.co.za>
2023-09-07 18:07:47 +0200

    Refactors ThemeToggle component to use NoSSR component

--- frontend/src/components/ThemeToggle.tsx
+++ frontend/src/components/ThemeToggle.tsx
@@ -1,17 +1,11 @@
 "use client"
-import { useState, useEffect } from "react"
-import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
-
+import { Moon, Sun } from "lucide-react"
 import { Button } from "@/components/ui/Button"
+import NoSSR from "./NoSSR"
 
-export default function ModeToggle() {
+function ModeToggle() {
   const { setTheme, resolvedTheme } = useTheme()
-  const [mounted, setMounted] = useState(false)
-
-  useEffect(() => {
-    setMounted(true)
-  }, [])
 
   const toggleTheme = () => {
     const newTheme = resolvedTheme === "light" ? "dark" : "light"
@@ -20,11 +14,17 @@ export default function ModeToggle() {
 
   return (
     <>
-      {mounted && (
-        <Button variant="outline" size="icon" onClick={toggleTheme}>
-          {resolvedTheme === "light" ? <Moon /> : <Sun />}
-        </Button>
-      )}
+      <Button variant="outline" size="icon" onClick={toggleTheme}>
+        {resolvedTheme === "light" ? <Moon /> : <Sun />}
+      </Button>
     </>
+  )
+}
+
+export default function () {
+  return (
+    <NoSSR>
+      <ModeToggle />
+    </NoSSR>
   )
 }
