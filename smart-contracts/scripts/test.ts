12e2c75ed44b0049ec1e68fc9691c5b15d32e656
Dennis Cheng <dennischeng@hotmail.co.za>
2023-09-17 22:53:23 +0200

    Remove unused files

--- smart-contracts/scripts/test.ts
+++
@@ -1,9 +0,0 @@
-import { ethers } from "hardhat"
-// ethers.toUtf8Bytes
-const { keccak256, toUtf8Bytes } = ethers
-
-function main() {
-    console.log(keccak256(toUtf8Bytes("symbol()")))
-}
-
-main()
