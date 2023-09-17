f3ebd995265501bcbdd2f5d991711177fe8dab94
Dennis Cheng <dennischeng@hotmail.co.za>
2023-09-17 22:47:55 +0200

    Add sepolia network configs

--- smart-contracts/hardhat.config.ts
+++ smart-contracts/hardhat.config.ts
@@ -3,7 +3,13 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
-  defaultNetwork: "localhost"
+  defaultNetwork: "localhost",
+  networks: {
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/cMmOs6R3rDjE_rEWWldzzCKkcl9VnYtQ",
+      accounts: [process.env.SEPOLIA_PRIVATE_KEY as string],
+    },
+  },
 };
 
 export default config;
