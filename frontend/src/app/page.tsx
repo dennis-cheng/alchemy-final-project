e67bb69bfe6a9ed126ab1655d43dc5184ff83d45
Dennis Cheng <dennischeng@hotmail.co.za>
2023-09-18 19:34:38 +0200

    update home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,11 +2,15 @@ export default function Home() {
   return (
     <section>
       <h1 className="text-4xl font-extrabold">Welcome to ZARC</h1>
-      <h2 className="text-3xl font-bold">Please use Sepolia Network</h2>
       <p className="leading-7 mt-2">
         ZARC is an ERC20 token implementation which also allows users to view
         their approvals and allowances
       </p>
+      <h3 className="text-2xl font-bold mt-1">Note</h3>
+      <ul>
+        <li>Use Sepolia testnet</li>
+        <li>Use MetaMask wallet extention</li>
+      </ul>
     </section>
   );
 }
